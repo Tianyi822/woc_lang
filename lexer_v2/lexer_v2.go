ba28c28a430dff8309a8ff0f805911a5ac341ee5
chentianyi <chentyit@163.com>
2023-01-31 19:35:11 +0800

    词法分析器: 1. 增加解析符号，可解析组合符号 2. 添加错误集，用于收集解析过程中产生的错误

--- lexer_v2/lexer_v2.go
+++ lexer_v2/lexer_v2.go
@@ -1,6 +1,7 @@
 package lexer_v2
 
 import (
+	"fmt"
 	"woc_lang/lexer_v2/dfa_state"
 	"woc_lang/token_v2"
 )
@@ -19,19 +20,25 @@ type Lexer struct {
 	reader_index int                // NextToken 方法中使用该指针指向当前需要读取的 Token
 	base_index   int                // 需解析 Token 的起始位置，每解析完一个 Token，更新 base_index
 	tokens       []token_v2.Token   // 收集解析出来的Token，按照顺序存储，向外提供函数访问
-	state        dfa_state.DfaState // 用于记录分析器当前的状态
+	cur_state    dfa_state.DfaState // 用于记录分析器当前的状态
+	errors       []string           // 用于收集错误信息
 }
 
 func New(input string) *Lexer {
 	l := &Lexer{
-		code:         []rune(input),
+		code:         append([]rune(input), '\n'),
 		reader_index: -1,
 		base_index:   0,
 		tokens:       []token_v2.Token{},
-		state:        dfa_state.Initial,
+		cur_state:    dfa_state.Initial,
+		errors:       []string{},
 	}
 	l.tokenize()
 	return l
+}
+
+func (l *Lexer) Errors() []string {
+	return l.errors
 }
 
 func (l *Lexer) NextToken() token_v2.Token {
@@ -46,18 +53,97 @@ func (l *Lexer) NextToken() token_v2.Token {
 // tokenize 识别代码中的 token
 func (l *Lexer) tokenize() {
 	for i, c := range l.code {
-		switch l.state {
+		switch l.cur_state {
 		case dfa_state.Initial: // 初始状态，读取第一个字符
 			l.stateTrans(i, c)
+
 		case dfa_state.Comma_State, // ,
 			dfa_state.Dot_State,                            // .
 			dfa_state.Semicolon_State,                      // ;
 			dfa_state.Colon_State,                          // :
 			dfa_state.Underline_State,                      // _
+			dfa_state.Add_State,                            // +
+			dfa_state.Asterisk_State,                       // *
+			dfa_state.Slash_State,                          // /
 			dfa_state.Lparen_State, dfa_state.Rparen_State, // ()
 			dfa_state.Lbrace_State, dfa_state.Rbrace_State, // {}
 			dfa_state.Lbracket_State, dfa_state.Rbracket_State: // []
 			l.stateTrans(i, c)
+
+		case dfa_state.Assign_State: // =
+			// 判断当前字符是不是 =
+			// 如果是，则修改状态为 EQ_State，表示该 Token 是两个 = 组成的判断相等的 Token
+			// 如果不是，则表示这只是一个赋值 Token
+			if c == '=' {
+				l.cur_state = dfa_state.Eq_State
+			} else {
+				l.addToken(i, token_v2.ASSIGN)
+			}
+		case dfa_state.Eq_State: // ==
+			l.addToken(i, token_v2.EQ)
+
+		case dfa_state.Minus_State: // -
+			if c == '>' {
+				l.cur_state = dfa_state.Arrow_State
+			} else {
+				l.addToken(i, token_v2.MINUS)
+			}
+		case dfa_state.Arrow_State: // ->
+			l.addToken(i, token_v2.ARROW)
+
+		case dfa_state.Bang_State: // !
+			if c == '=' {
+				l.cur_state = dfa_state.Neq_State
+			} else {
+				l.addToken(i, token_v2.BANG)
+			}
+		case dfa_state.Neq_State: // !=
+			l.addToken(i, token_v2.NEQ)
+
+		case dfa_state.Gt_State:
+			if c == '=' {
+				l.cur_state = dfa_state.Ge_State
+			} else if c == '>' {
+				l.cur_state = dfa_state.Bit_R_Offset_State
+			} else {
+				l.addToken(i, token_v2.GT)
+			}
+		case dfa_state.Ge_State:
+			l.addToken(i, token_v2.GE)
+		case dfa_state.Bit_R_Offset_State:
+			l.addToken(i, token_v2.BIT_R_OFFSET)
+
+		case dfa_state.Lt_State:
+			if c == '=' {
+				l.cur_state = dfa_state.Le_State
+			} else if c == '<' {
+				l.cur_state = dfa_state.Bit_L_Offset_State
+			} else {
+				l.addToken(i, token_v2.LT)
+			}
+		case dfa_state.Le_State:
+			l.addToken(i, token_v2.LE)
+		case dfa_state.Bit_L_Offset_State:
+			l.addToken(i, token_v2.BIT_L_OFFSET)
+
+		case dfa_state.Bit_And_State:
+			if c == '&' {
+				l.cur_state = dfa_state.And_State
+			} else {
+				l.addToken(i, token_v2.BIT_AND)
+			}
+		case dfa_state.And_State:
+			l.addToken(i, token_v2.AND)
+
+		case dfa_state.Bit_Or_State:
+			if c == '|' {
+				l.cur_state = dfa_state.Or_State
+			} else {
+				l.addToken(i, token_v2.BIT_OR)
+			}
+		case dfa_state.Or_State:
+			l.addToken(i, token_v2.OR)
+
 		default:
 			// 未定义状态的就是空字符
 			continue
@@ -70,47 +156,139 @@ func (l *Lexer) tokenize() {
 // stateTrans 状态转移处理
 // 有限状态机一开始就进入初始状态,这个初始状态其实并不做停留，它马上进入其他状态。
 // 某个 Token 解析完毕，也进入初始状态，在这里把 Token 记下来，然后建立一个新的 Token
-func (l *Lexer) stateTrans(i int, ch rune) {
-	// 这里 +1 是为了从 code 中切出来正确的 Token
-	end_index := i + 1
+func (l *Lexer) stateTrans(end_index int, ch rune) {
+	// 如果当前字符的 ASCII 码为 0，则表示已经到达末尾，修改状态为结束态
+	if ch == 0 {
+		l.addEOFToken()
+		return
+	}
+
 	switch ch {
 	case ',':
 		// 将解析到的 Token 添加到集合中，并修改分析器状态
-		l.state = dfa_state.Comma_State
-		l.addToken(token_v2.COMMA, string(l.code[l.base_index:end_index]))
-		l.base_index = end_index
+		l.cur_state = dfa_state.Comma_State
+		l.addToken(end_index, token_v2.COMMA)
+
 	case '.':
-		l.state = dfa_state.Dot_State
-		l.addToken(token_v2.DOT, string(l.code[l.base_index:end_index]))
-		l.base_index = end_index
+		l.cur_state = dfa_state.Dot_State
+		l.addToken(end_index, token_v2.DOT)
+
 	case ';':
-		l.state = dfa_state.Semicolon_State
-		l.addToken(token_v2.SEMICOLON, string(l.code[l.base_index:end_index]))
-		l.base_index = end_index
+		l.cur_state = dfa_state.Semicolon_State
+		l.addToken(end_index, token_v2.SEMICOLON)
+
 	case ':':
-		l.state = dfa_state.Colon_State
-		l.addToken(token_v2.COLON, string(l.code[l.base_index:end_index]))
-		l.base_index = end_index
+		l.cur_state = dfa_state.Colon_State
+		l.addToken(end_index, token_v2.COLON)
+
 	case '_':
-		l.state = dfa_state.Underline_State
-		l.addToken(token_v2.UNDERLINE, string(l.code[l.base_index:end_index]))
-		l.base_index = end_index
-	case 0:
-		l.state = dfa_state.End
-		l.addToken(token_v2.EOF, string(rune(0)))
+		l.cur_state = dfa_state.Underline_State
+		l.addToken(end_index, token_v2.UNDERLINE)
+
+	case '+':
+		l.cur_state = dfa_state.Add_State
+		l.addToken(end_index, token_v2.ADD)
+
+	case '*':
+		l.cur_state = dfa_state.Asterisk_State
+		l.addToken(end_index, token_v2.ASTERISK)
+
+	case '/':
+		l.cur_state = dfa_state.Slash_State
+		l.addToken(end_index, token_v2.SLASH)
+
+	case '(':
+		l.cur_state = dfa_state.Lparen_State
+		l.addToken(end_index, token_v2.LPAREN)
+
+	case ')':
+		l.cur_state = dfa_state.Rparen_State
+		l.addToken(end_index, token_v2.RPAREN)
+
+	case '[':
+		l.cur_state = dfa_state.Lbracket_State
+		l.addToken(end_index, token_v2.LBRACKET)
+
+	case ']':
+		l.cur_state = dfa_state.Rbracket_State
+		l.addToken(end_index, token_v2.RBRACKET)
+
+	case '{':
+		l.cur_state = dfa_state.Lbrace_State
+		l.addToken(end_index, token_v2.LBRACE)
+
+	case '}':
+		l.cur_state = dfa_state.Rbrace_State
+		l.addToken(end_index, token_v2.RBRACE)
+
+	case '-':
+		l.cur_state = dfa_state.Minus_State
+
+	case '=':
+		l.cur_state = dfa_state.Assign_State
+
+	case '!':
+		l.cur_state = dfa_state.Bang_State
+
+	case '>':
+		l.cur_state = dfa_state.Gt_State
+
+	case '<':
+		l.cur_state = dfa_state.Lt_State
+
+	case '&':
+		l.cur_state = dfa_state.Bit_And_State
+
+	case '|':
+		l.cur_state = dfa_state.Bit_Or_State
 	}
 }
 
 // addToken 将解析出来的 token 添加到集合中
+// param end_index: Token 字符结束位置
 // param tokenType: Token 类型
-// param literal: Token 字面量
-func (l *Lexer) addToken(tokenType token_v2.TokenType, literal string) {
-	tok := token_v2.Token{
-		Type:    tokenType,
-		Literal: literal,
+func (l *Lexer) addToken(end_index int, tokenType token_v2.TokenType) {
+	// 消除 Token 中的空格，举个栗子：
+	// 有一个字符串 (= ==), 要从中解析出 (=)，按照当前的代码逻辑，end_index 指向第一个空格
+	// addToken 需要将 (= ) 修正为 (=)，这样就可以放心处理空格问题，无需在前面的逻辑中添加复杂代码
+	// 至于为什么不用 golang 提供的内置函数，是为了减少性能消耗，因为将 rune 数组转换为字符串会有一定的损耗
+	var realToken []rune
+	for i := l.base_index; i < end_index+1; i++ {
+		if !isBlank(l.code[i]) {
+			realToken = append(realToken, l.code[i])
+		}
 	}
 
+	tokLiteral := string(realToken)
+	if !checkToken(tokLiteral) {
+		msg := fmt.Sprintf("该符号/关键字未定义，请检查代码是否有误: %s", tokLiteral)
+		l.errors = append(l.errors, msg)
+	}
+
+	tok := token_v2.Token{
+		Type:    tokenType,
+		Literal: tokLiteral,
+	}
 	l.tokens = append(l.tokens, tok)
+	l.base_index = end_index + 1
+
+	// 当一个 Token 添加到集合后，就需要重置状态
+	l.cur_state = dfa_state.Initial
+}
+
+// checkToken 检查是否有定义此类型 Token
+func checkToken(tokLiteral string) bool {
+	_, ok := token_v2.TokenMap[tokLiteral]
+	return ok
+}
+
+// addEOFToken 添加结束 Token
+func (l *Lexer) addEOFToken() {
+	l.cur_state = dfa_state.End
+	l.tokens = append(l.tokens, token_v2.Token{
+		Type:    token_v2.EOF,
+		Literal: "",
+	})
 }
 
 // isDigit 判断是否是数字
