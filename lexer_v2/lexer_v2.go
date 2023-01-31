96939a05563499078e27d9af7d172da413cdb508
chentianyi <chentyit@163.com>
2023-01-31 11:56:04 +0800

    词法分析器: 1. 添加分析逻辑，初步逻辑结构完成

--- lexer_v2/lexer_v2.go
+++ lexer_v2/lexer_v2.go
@@ -15,36 +15,90 @@ import (
 // Lexer 第二版词法分析器，相较于第一版本的分析器，添加了状态检查
 // 将解析过程规范化，基于 '确定有限自动机' 原理实现，
 type Lexer struct {
-	code       []rune           // 需要解析的代码
-	base_index int              // 需解析 Token 的起始位置，每解析完一个 Token，更新 base_index
-	tokens     []token_v2.Token // 收集解析出来的Token，按照顺序存储，向外提供函数访问
+	code         []rune             // 需要解析的代码
+	reader_index int                // NextToken 方法中使用该指针指向当前需要读取的 Token
+	base_index   int                // 需解析 Token 的起始位置，每解析完一个 Token，更新 base_index
+	tokens       []token_v2.Token   // 收集解析出来的Token，按照顺序存储，向外提供函数访问
+	state        dfa_state.DfaState // 用于记录分析器当前的状态
 }
 
 func New(input string) *Lexer {
 	l := &Lexer{
-		code:       []rune(input),
-		base_index: 0,
-		tokens:     []token_v2.Token{},
+		code:         []rune(input),
+		reader_index: -1,
+		base_index:   0,
+		tokens:       []token_v2.Token{},
+		state:        dfa_state.Initial,
 	}
 	l.tokenize()
 	return l
 }
 
+func (l *Lexer) NextToken() token_v2.Token {
+	if l.reader_index >= len(l.tokens) {
+		return token_v2.Token{Type: token_v2.EOF}
+	} else {
+		l.reader_index += 1
+		return l.tokens[l.reader_index]
+	}
+}
+
+// tokenize 识别代码中的 token
 func (l *Lexer) tokenize() {
-	state := dfa_state.Initial
-	for _, c := range l.code {
-		switch state {
+	for i, c := range l.code {
+		switch l.state {
 		case dfa_state.Initial: // 初始状态，读取第一个字符
-			state = l.stateTrans(c)
+			l.stateTrans(i, c)
+		case dfa_state.Comma_State, // ,
+			dfa_state.Dot_State,                            // .
+			dfa_state.Semicolon_State,                      // ;
+			dfa_state.Colon_State,                          // :
+			dfa_state.Underline_State,                      // _
+			dfa_state.Lparen_State, dfa_state.Rparen_State, // ()
+			dfa_state.Lbrace_State, dfa_state.Rbrace_State, // {}
+			dfa_state.Lbracket_State, dfa_state.Rbracket_State: // []
+			l.stateTrans(i, c)
+		default:
+			// 未定义状态的就是空字符
+			continue
 		}
 	}
+	// 扫描结束后，在 Token 集合中添加 EOF Token 表示结束
+	l.stateTrans(len(l.code), 0)
 }
 
 // stateTrans 状态转移处理
 // 有限状态机一开始就进入初始状态,这个初始状态其实并不做停留，它马上进入其他状态。
 // 某个 Token 解析完毕，也进入初始状态，在这里把 Token 记下来，然后建立一个新的 Token
-func (l *Lexer) stateTrans(ch rune) dfa_state.DfaState {
-	return dfa_state.Initial
+func (l *Lexer) stateTrans(i int, ch rune) {
+	// 这里 +1 是为了从 code 中切出来正确的 Token
+	end_index := i + 1
+	switch ch {
+	case ',':
+		// 将解析到的 Token 添加到集合中，并修改分析器状态
+		l.state = dfa_state.Comma_State
+		l.addToken(token_v2.COMMA, string(l.code[l.base_index:end_index]))
+		l.base_index = end_index
+	case '.':
+		l.state = dfa_state.Dot_State
+		l.addToken(token_v2.DOT, string(l.code[l.base_index:end_index]))
+		l.base_index = end_index
+	case ';':
+		l.state = dfa_state.Semicolon_State
+		l.addToken(token_v2.SEMICOLON, string(l.code[l.base_index:end_index]))
+		l.base_index = end_index
+	case ':':
+		l.state = dfa_state.Colon_State
+		l.addToken(token_v2.COLON, string(l.code[l.base_index:end_index]))
+		l.base_index = end_index
+	case '_':
+		l.state = dfa_state.Underline_State
+		l.addToken(token_v2.UNDERLINE, string(l.code[l.base_index:end_index]))
+		l.base_index = end_index
+	case 0:
+		l.state = dfa_state.End
+		l.addToken(token_v2.EOF, string(rune(0)))
+	}
 }
 
 // addToken 将解析出来的 token 添加到集合中
--- lexer_v2/lexer_v2_test.go
+++ lexer_v2/lexer_v2_test.go
@@ -2,29 +2,36 @@ package lexer_v2
 
 import (
 	"testing"
-	"woc_lang/token"
+	"woc_lang/token_v2"
 )
 
 func TestToken(t *testing.T) {
 	tt := struct {
-		input    string
-		expected []token.Token
+		input          string
+		expectedTokens []token_v2.Token
 	}{
-		",.;:()[]{}|",
-		[]token.Token{
-			{token.COMMA, ","},
-			{token.DOT, "."},
-			{token.SEMICOLON, ";"},
-			{token.COLON, ":"},
-			{token.LPAREN, "("},
-			{token.RPAREN, ")"},
-			{token.LBRACKET, "["},
-			{token.RBRACKET, "]"},
-			{token.LBRACE, "{"},
-			{token.RBRACE, "}"},
-			{token.OR, "|"},
+		",.;:_",
+		[]token_v2.Token{
+			{token_v2.COMMA, ","},
+			{token_v2.DOT, "."},
+			{token_v2.SEMICOLON, ";"},
+			{token_v2.COLON, ":"},
+			//{token_v2.LPAREN, "("},
+			//{token_v2.RPAREN, ")"},
+			//{token_v2.LBRACKET, "["},
+			//{token_v2.RBRACKET, "]"},
+			//{token_v2.LBRACE, "{"},
+			//{token_v2.RBRACE, "}"},
 		},
 	}
 
-	New(tt.input)
+	l := New(tt.input)
+
+	for i, expTok := range tt.expectedTokens {
+		tok := l.NextToken()
+		if tok.Literal != expTok.Literal || tok.Type != expTok.Type {
+			t.Fatalf("第 %d 个 token 解析错误:\n期望: %v\n实际: %v",
+				i+1, expTok, tok)
+		}
+	}
 }
