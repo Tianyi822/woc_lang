9d716284e3f442f45eb726a880ad6e0c7fc526d6
chentianyi <chentyit@163.com>
2023-02-19 17:39:12 +0800

    语法分析器: 1.修改 LOW_EST 为 LOW_EST_LEVEL; 2.添加 '-' 和 '!' 优先级

--- parser/priority.go
+++ parser/priority.go
@@ -3,18 +3,21 @@ package parser
 import "woc_lang/token"
 
 const (
-	LOW_EST = iota
+	LOW_EST_LEVEL = iota
 	LEVEL_1
 	LEVEL_2
 	LEVEL_3
+	PREFIX_LEVEL
 )
 
 var prioritiesTable = map[token.TokenType]int{
-	token.EQ:  LEVEL_1,
-	token.NEQ: LEVEL_1,
-	token.LT:  LEVEL_2,
-	token.GT:  LEVEL_2,
-	token.LE:  LEVEL_2,
-	token.GE:  LEVEL_2,
-	token.ADD: LEVEL_3,
+	token.EQ:    LEVEL_1,
+	token.NEQ:   LEVEL_1,
+	token.LT:    LEVEL_2,
+	token.GT:    LEVEL_2,
+	token.LE:    LEVEL_2,
+	token.GE:    LEVEL_2,
+	token.ADD:   LEVEL_3,
+	token.MINUS: PREFIX_LEVEL,
+	token.BANG:  PREFIX_LEVEL,
 }
