a5538731ead31f24dac6cc3a1d20418fbaf13ea9
chentianyi <chentyit@163.com>
2023-03-20 16:31:49 +0800

    语法分析器测试用例: TestOperatorPrecedenceParsing 测试用例添加新代码

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -193,6 +193,22 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 			"-(5 + 5);",
 			"(-(5 + 5));",
 		},
+		{
+			"!(true == true);",
+			"(!(true == true));",
+		},
+		{
+			"a + add(b * c) + d;",
+			"((a + add((b * c))) + d);",
+		},
+		{
+			"add(a, b, 1, 2 * 3, 4 + 5, add(6, 7 * 8));",
+			"add(a, b, 1, (2 * 3), (4 + 5), add(6, (7 * 8)));",
+		},
+		{
+			"add(a + b + c * d / f + g);",
+			"add((((a + b) + ((c * d) / f)) + g));",
+		},
 	}
 
 	for _, tt := range tests {
