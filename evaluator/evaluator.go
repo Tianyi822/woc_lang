acf8112f1cd403ced5c3b7c9231468722e9aa176
chentianyi <chentyit@163.com>
2023-04-21 21:51:41 +0800

    抽象语法树求值器: 1. 实现前缀表达式求值

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -20,6 +20,10 @@ func Eval(node ast.Node) object.Object {
 	case *ast.ExpressionStatement: // 解析表达式语句
 		return Eval(node.Expression)
 
+	case *ast.PrefixExpression:
+		right := Eval(node.Right) // 解析前缀表达式右侧表达式
+		return evalPrefixExpression(node.Operator, right)
+
 	case *ast.IntegerLiteral: // 解析整数字面量
 		return &object.Integer{Value: node.Value}
 
@@ -45,4 +49,40 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+// evalPrefixExpression 对前缀表达式进行求值
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+// evalBangOperatorExpression 对取反前缀表达式中的进行求值
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+// evalMinusOperatorExpression 对取负前缀表达式中的进行求值
+func evalMinusOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	val := right.(*object.Integer).Value
+	return &object.Integer{Value: -val}
 }
