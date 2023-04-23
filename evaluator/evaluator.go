2410fa7b99a2e0d3f3c21a1f39d9a9f4d8df330f
chentianyi <chentyit@163.com>
2023-04-23 22:25:03 +0800

    抽象语法树求值器: 1. 实现中缀表达式求值

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -24,6 +24,11 @@ func Eval(node ast.Node) object.Object {
 		right := Eval(node.Right) // 解析前缀表达式右侧表达式
 		return evalPrefixExpression(node.Operator, right)
 
+	case *ast.InfixExpression:
+		left := Eval(node.LeftExp)   // 解析中缀表达式左侧表达式
+		right := Eval(node.RightExp) // 解析中缀表达式右侧表达式
+		return evalInfixExpression(node.Operator, left, right)
+
 	case *ast.IntegerLiteral: // 解析整数字面量
 		return &object.Integer{Value: node.Value}
 
@@ -58,6 +63,45 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 		return evalBangOperatorExpression(right)
 	case "-":
 		return evalMinusOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalInfixExpression(operator string, left, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBoolToBooleanObject(left == right)
+	case operator == "!=":
+		return nativeBoolToBooleanObject(left != right)
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(operator string, left, right object.Object) object.Object {
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+
+	switch operator {
+	case "+":
+		return &object.Integer{Value: leftVal + rightVal}
+	case "-":
+		return &object.Integer{Value: leftVal - rightVal}
+	case "*":
+		return &object.Integer{Value: leftVal * rightVal}
+	case "/":
+		return &object.Integer{Value: leftVal / rightVal}
+	case "<":
+		return nativeBoolToBooleanObject(leftVal < rightVal)
+	case ">":
+		return nativeBoolToBooleanObject(leftVal > rightVal)
+	case "==":
+		return nativeBoolToBooleanObject(leftVal == rightVal)
+	case "!=":
+		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
 	}
