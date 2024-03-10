4452e34253fe4b6402e93aff2b721b8fc0c7c33f
Tianyi <chentyit@163.com>
2024-03-10 22:34:11 +0800

    Add some comments and 'to_string' for 'Node' trait.

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,29 +1,47 @@
+use std::cell::RefCell;
+
 pub mod statement;
 pub mod expression;
 
+// This module is used to define the AST node.
 pub trait Node {
+    // This method returns the token literal.
     fn token_literal(&self) -> String;
+
+    // This method returns the content of the node as a string.
+    fn to_string(&self) -> String;
 }
 
+// This trait is used to define the statement node.
+// e.g. let a = 1; function add(a, b) { a + b; } etc.
 pub trait Statement: Node {
+    // This method just marks the struct as a statement node.
     fn statement_node(&self);
 }
 
+// This trait is used to define the expression node.
+// e.g. 1 + 1; if (a > 1) { a + 1; } etc.
 pub trait Expression: Node {
+    // This method just marks the struct as an expression node.
     fn expression_node(&self);
 }
 
 // This struct stores the program information that the parser will analyze.
 // In the same time, this is the root node of the AST.
 pub struct Program {
-    pub statements: Vec<Box<dyn Statement>>,
+    pub statements: RefCell<Vec<Box<dyn Statement>>>,
 }
 
 impl Program {
     pub fn new() -> Program {
         Program {
-            statements: Vec::new(),
+            statements: RefCell::new(Vec::new())
         }
+    }
+
+    // Add an AST node.
+    pub fn push(&self, statement: Box<dyn Statement>) {
+        self.statements.borrow_mut().push(statement);
     }
 }
 
@@ -34,5 +52,15 @@ impl Node for Program {
         } else {
             String::from("")
         }
+    }
+
+    fn to_string(&self) -> String {
+        let mut out = String::new();
+
+        for statement in self.statements.iter() {
+            out.push_str(&statement.to_string());
+        }
+
+        out
     }
 }
