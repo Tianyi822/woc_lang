5bb331ec4ed077c5db5c3fb457961a4b945c594e
chentianyi <chentyit@163.com>
2024-03-21 19:45:40 +0800

    Add number ast node.

--- src/ast/expression.rs
+++ src/ast/expression.rs
@@ -26,3 +26,37 @@ impl Node for IdentifierExp {
 impl Expression for IdentifierExp {
     fn expression_node(&self) {}
 }
+
+// This struct is used to represent the number expression.
+pub struct NumExp {
+    token: Token,
+    i_value: Option<i64>,
+    f_value: Option<f64>,
+}
+
+impl NumExp {
+    pub fn new(token: Token, i_value: Option<i64>, f_value: Option<f64>) -> NumExp {
+        NumExp {
+            token,
+            i_value,
+            f_value,
+        }
+    }
+}
+
+impl Node for NumExp {
+    fn token_literal(&self) -> String {
+        self.token.literal().to_string()
+    }
+
+    fn to_string(&self) -> String {
+        match self.i_value {
+            Some(i) => i.to_string(),
+            None => self.f_value.unwrap().to_string(),
+        }
+    }
+}
+
+impl Expression for NumExp {
+    fn expression_node(&self) {}
+}
