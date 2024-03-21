624aee5234dee0ecd45bba27429e412703b0da37
chentianyi <chentyit@163.com>
2024-03-21 19:25:55 +0800

    Add "LEVEL_7" precedence.

--- src/token/precedence.rs
+++ src/token/precedence.rs
@@ -1,4 +1,4 @@
-type Precedence = u32;
+pub type Precedence = u32;
 
 pub const LEVEL_0: Precedence = 0;
 pub const LEVEL_1: Precedence = 1;
@@ -7,3 +7,4 @@ pub const LEVEL_3: Precedence = 3;
 pub const LEVEL_4: Precedence = 4;
 pub const LEVEL_5: Precedence = 5;
 pub const LEVEL_6: Precedence = 6;
+pub const LEVEL_7: Precedence = 7;
