2749b667aa20f3bff1aaa584017b014f042083d8
Tianyi <chentyit@163.com>
2024-05-26 22:39:32 +0800

    Add file information to "Token".

--- tests/token_test.rs
+++ tests/token_test.rs
@@ -1,31 +1,31 @@
 #[cfg(test)]
 mod token_test {
-    use woc_lang::token::token::{Token, TokenType};
+    use woc_lang::token::{token::Token, types::TokenType};
 
     #[test]
     fn test_new() {
-        let token = Token::new(TokenType::For, "for");
+        let token = Token::new(TokenType::For, "for", "", 0);
         assert_eq!(token.token_type(), &TokenType::For);
         assert_eq!(token.literal(), "for");
     }
 
     #[test]
     fn test_is_eof() {
-        let token = Token::new(TokenType::Eof, "");
+        let token = Token::new(TokenType::Eof, "", "", 0);
         assert_eq!(token.is_eof(), true);
     }
 
     #[test]
     fn test_priority() {
-        let token = Token::new(TokenType::Or, "or");
+        let token = Token::new(TokenType::Or, "or", "", 0);
         assert_eq!(token.precedence(), 1);
-        let token = Token::new(TokenType::And, "and");
+        let token = Token::new(TokenType::And, "and", "", 0);
         assert_eq!(token.precedence(), 2);
     }
 
     #[test]
     fn test_float() {
-        let token = Token::new(TokenType::FloatNum, "1.0");
+        let token = Token::new(TokenType::FloatNum, "1.0", "", 0);
         assert_eq!(token.token_type(), &TokenType::FloatNum);
         assert_eq!(token.literal(), "1.0");
     }
